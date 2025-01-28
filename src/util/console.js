4b5e12fcf961994a138ac24761bff726861679db
BeatBroccoli <93484781+BeatBroccoli@users.noreply.github.com>
2025-01-29 00:38:28 +0100

    Update console.js to run in enviroments without console
    I tried to run this component on NUXT SSR but it could not be served by Cloudflare because in that context the console is not reachable so an easy fix is just to provide an empty stub object

--- src/util/console.js
+++ src/util/console.js
@@ -1,9 +1,9 @@
 function getConsole() {
-  if (typeof window !== "undefined") {
-    return window.console;
-  }
-  return global.console;
+  if (typeof window !== "undefined" && window.console) return window.console;
+  if (global && global.console) return global.console;
+  return { log: function () { }, warn: function () { }, error: function () { }, info: function () { } }
 }
+
 const console = getConsole();
 
 export { console };
